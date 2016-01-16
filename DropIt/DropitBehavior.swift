f11b732d32ca444e8fde45d5e91b40a63e02d4d1
hanjoes <hanzhou87@gmail.com>
2016-01-17 00:00:28 +0800

    Added code.

--- DropIt/DropitBehavior.swift
+++ DropIt/DropitBehavior.swift
@@ -6,8 +6,50 @@
 //  Copyright © 2016 USF. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 
+// This class encapsulates all the behavior we want for the animation 
+// in the game. And when adding a view to the behavior we also add
+// the view to the super view that contains the behavior.
 class DropitBehavior: UIDynamicBehavior {
-
+    let gravity = UIGravityBehavior()
+    
+    lazy var collider: UICollisionBehavior = {
+        let lazilyCreatedCollider = UICollisionBehavior()
+        lazilyCreatedCollider.translatesReferenceBoundsIntoBoundary = true
+        return lazilyCreatedCollider
+    }()
+    
+    lazy var dropBehavior: UIDynamicItemBehavior = {
+        let lazilyCreatedDropBehavior = UIDynamicItemBehavior()
+        lazilyCreatedDropBehavior.allowsRotation = true
+        lazilyCreatedDropBehavior.elasticity = 0.75
+        return lazilyCreatedDropBehavior
+    }()
+    
+    override init() {
+        super.init()
+        addChildBehavior(gravity)
+        addChildBehavior(collider)
+        addChildBehavior(dropBehavior)
+    }
+    
+    func addBarrier(path: UIBezierPath, named name: String) {
+        collider.removeBoundaryWithIdentifier(name)
+        collider.addBoundaryWithIdentifier(name, forPath: path)
+    }
+    
+    func addDrop(drop: UIView) {
+        dynamicAnimator?.referenceView?.addSubview(drop)
+        gravity.addItem(drop)
+        collider.addItem(drop)
+        dropBehavior.addItem(drop)
+    }
+    
+    func removeDrop(drop: UIView) {
+        gravity.removeItem(drop)
+        collider.removeItem(drop)
+        dropBehavior.removeItem(drop)
+        drop.removeFromSuperview()
+    }
 }
--- DropIt/DropitViewController.swift
+++ DropIt/DropitViewController.swift
@@ -6,8 +6,108 @@
 //  Copyright © 2016 USF. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 
-class DropitViewController: UIViewController {
+class DropitViewController: UIViewController, UIDynamicAnimatorDelegate {
+    @IBOutlet weak var gameView: UIView!
+    @IBAction func drop(sender: UITapGestureRecognizer) {
+        drop()
+    }
+    
+    let dropitBehavior = DropitBehavior()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        animator.addBehavior(dropitBehavior)
+    }
 
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let barrierSize = dropSize
+        let barrierOrigin = CGPoint(x: gameView.bounds.midX-barrierSize.width/2, y: gameView.bounds.midY-barrierSize.height/2)
+        let path = UIBezierPath(ovalInRect: CGRect(origin: barrierOrigin, size: barrierSize))
+        dropitBehavior.addBarrier(path, named: PathNames.MiddleBarrier)
+    }
+    
+    // Be careful about where to call this computed property.
+    lazy var animator: UIDynamicAnimator = {
+        let lazilyCreatedDynamicAnimator = UIDynamicAnimator(referenceView: self.gameView)
+        lazilyCreatedDynamicAnimator.delegate = self
+        return lazilyCreatedDynamicAnimator
+    }()
+    
+    func dynamicAnimatorDidPause(animator: UIDynamicAnimator) {
+        removeCompleteRow()
+    }
+    
+    var dropsPerRow = 10
+    var dropSize: CGSize {
+        let size = gameView.bounds.size.width / CGFloat(dropsPerRow)
+        return CGSize(width: size, height: size)
+    }
+    
+    func drop() {
+        var frame = CGRect(origin: CGPointZero, size: dropSize)
+        frame.origin.x = CGFloat.random(dropsPerRow) * dropSize.width
+        
+        let dropView = UIView(frame: frame)
+        dropView.backgroundColor = UIColor.random
+        // The item wil start to animate.
+        dropitBehavior.addDrop(dropView)
+    }
+    
+    func removeCompleteRow() {
+        var dropsToRemove = [UIView]()
+        var dropFrame = CGRect(x: 0, y: gameView.frame.maxY, width: dropSize.width, height: dropSize.height)
+        
+        repeat {
+            dropFrame.origin.y -= dropSize.height
+            dropFrame.origin.x = 0
+            var dropsFound = [UIView]()
+            var rowIsComplete = true
+            for _ in 0 ..< dropsPerRow {
+                if let hitView = gameView.hitTest(CGPoint(x: dropFrame.midX, y: dropFrame.midY), withEvent: nil) {
+                    if hitView.superview == gameView {
+                        dropsFound.append(hitView)
+                    }
+                    else {
+                        rowIsComplete = false
+                    }
+                    dropFrame.origin.x += dropSize.width
+                }
+            }
+            if rowIsComplete {
+                dropsToRemove += dropsFound
+            }
+        } while dropsToRemove.count == 0 && dropFrame.origin.y > 0
+        
+        for drop in dropsToRemove {
+            dropitBehavior.removeDrop(drop)
+        }
+        
+    }
+}
+
+private extension CGFloat {
+    static func random(max: Int) -> CGFloat {
+        return CGFloat(arc4random() % UInt32(max))
+    }
+}
+
+private extension UIColor {
+    class var random: UIColor {
+        switch arc4random()%5 {
+        case 0: return UIColor.greenColor()
+        case 1: return UIColor.blueColor()
+        case 2: return UIColor.orangeColor()
+        case 3: return UIColor.redColor()
+        case 4: return UIColor.purpleColor()
+        default: return UIColor.blackColor()
+        }
+    }
+}
+
+private struct PathNames {
+    static let MiddleBarrier = "Middle Barrier"
 }
